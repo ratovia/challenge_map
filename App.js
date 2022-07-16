847cc7ce08cd7d3f28e196f942b69a314d697889
ratovia <ratovia@hotmail.co.jp>
2022-07-16 20:37:07 +0900

    react native navigationを導入

--- App.js
+++ App.js
@@ -1,30 +1,5 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, View, FlatList, SafeAreaView } from "react-native"
-import { ListItem } from "./components/ListItem"
-import { prefecture } from "./prefecture"
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-})
+import { AppNavigator } from "./navigation/AppNavigator"
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={prefecture}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl="https://picsum.photos/100/100"
-            prefecture={item.name}
-            achievement={20}
-          />
-        )}
-        keyExtractor={(pre) => pre.id}
-      ></FlatList>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  )
+  return <AppNavigator />
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, FlatList, SafeAreaView } from "react-native"
+import { ListItem } from "../components/ListItem"
+import { prefecture } from "../prefecture"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+})
+
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={prefecture}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl="https://picsum.photos/100/100"
+            prefecture={item.name}
+            achievement={20}
+          />
+        )}
+        keyExtractor={(pre) => pre.id}
+      ></FlatList>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  )
+}
