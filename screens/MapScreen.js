52cbcf86a0c1ed20206f78c3519e062f67f4f1f5
ratovia <ratovia@hotmail.co.jp>
2022-07-17 18:06:22 +0900

    d3js svgでの線の描画

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, SafeAreaView, Text, View } from "react-native"
 import Svg, { Path } from "react-native-svg"
+import * as d3 from "d3"
 
 const styles = StyleSheet.create({
   container: {
@@ -10,12 +11,32 @@ const styles = StyleSheet.create({
 })
 
 export const MapScreen = () => {
+  // Pathの座標データ
+  const data = [
+    { x: 0, y: 20 },
+    { x: 150, y: 250 },
+    { x: 300, y: 200 },
+    { x: 450, y: 20 },
+    { x: 600, y: 380 },
+  ]
+
+  // line作成関数
+  const curveFunc = d3
+    .line()
+    .curve(d3.curveBasis) // curveメソッドで線の形を変更
+    .x(function (d) {
+      return d.x
+    })
+    .y(function (d) {
+      return d.y
+    })
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>hello</Text>
       <View style={styles.container}>
-        <Svg width={20} height={20} viewBox="0 0 20 20">
-          <Path d="M16.993 6.667H3.227l6.883 6.883 6.883-6.883z" fill="#000" />
+        <Svg width={300} height={400}>
+          <Path d={curveFunc(data)} fill="none" stroke="#333" />
         </Svg>
       </View>
       <StatusBar style="auto" />
