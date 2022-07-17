a9618e8f1856dea11c8e731e788c2f530be0420d
ratovia <ratovia@hotmail.co.jp>
2022-07-17 17:31:35 +0900

    svgの描画

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, SafeAreaView, Text } from "react-native"
+import { StyleSheet, SafeAreaView, Text, View } from "react-native"
+import Svg, { Path } from "react-native-svg"
 
 const styles = StyleSheet.create({
   container: {
@@ -12,6 +13,11 @@ export const MapScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>hello</Text>
+      <View style={styles.container}>
+        <Svg width={20} height={20} viewBox="0 0 20 20">
+          <Path d="M16.993 6.667H3.227l6.883 6.883 6.883-6.883z" fill="#000" />
+        </Svg>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   )
